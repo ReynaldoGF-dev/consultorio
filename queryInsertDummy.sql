c00d41818142745d64961fb6ba0a80c37d4703f9
ReynaldoGF-dev <reynaldo-oso@hotmail.com>
2025-05-09 14:20:59 -0600

    insertDoctoresConsultorios

---
+++ queryInsertDummy.sql
@@ -0,0 +1,15 @@
+INSERT INTO doctores (nombre, apellido_paterno, apellido_materno, especialidad) VALUES
+('Pedro', 'Ramos', 'González', 'Medicina Interna'),
+('María', 'López', 'Fernández', 'Cardiología'),
+('Juan', 'Martínez', 'Rodríguez', 'Neurología'),
+('Roberto', 'Hernández', 'Díaz', 'Medicina Interna');
+
+INSERT INTO consultorios (numero_consultorio, piso) VALUES
+('101', '1'),
+('102', '1'),
+('201', '2'),
+('202', '2'),
+('301', '3');
+
+select * from citas;
+
